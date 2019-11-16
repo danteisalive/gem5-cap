fc18b8bfeaad8570308470c9f35ec5f4ad414a32
Rasool <arasool.sh2000@gmail.com>
2019-11-16 12:26:00 -0500

    Diasbling LVPT Stride!
    Change-Id: I2c6dad1e02c03118372644dd4f1a87b2dfb2cd1b

--- src/cpu/pred/lvpt.cc
+++ src/cpu/pred/lvpt.cc
@@ -332,7 +332,8 @@ DefaultLVPT::update(Addr instPC,
     // std::cout << std::hex << "Inst. " << instPC << " updated the LVPT." <<
     // " Before: " <<   lvpt[lvpt_idx].target << " After: " << target << "\n";
 
-    localBiases[lvpt_idx] = target.getPID() - lvpt[lvpt_idx].target.getPID();
+    //localBiases[lvpt_idx] = target.getPID() - lvpt[lvpt_idx].target.getPID();
+    localBiases[lvpt_idx] = 0;
     lvpt[lvpt_idx].tid = tid;
     lvpt[lvpt_idx].valid = true;
     lvpt[lvpt_idx].target = target;
